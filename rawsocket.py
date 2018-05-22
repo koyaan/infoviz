8dba01c35fb58a6ef005756f9127e77c0306a496
Armin Weihbold <armin.weihbold@gmail.com>
2018-05-22 23:16:25 +0200

    rawsocket trades poc

--- rawsocket.py
+++ rawsocket.py
@@ -1,15 +1,19 @@
 import asyncio
 import websockets
+import json
 
 @asyncio.coroutine
 def hello():
     websocket = yield from websockets.connect('wss://api.bitfinex.com/ws/2')
     yield from websocket.send('{"event":"subscribe","channel":"trades","symbol":"BTCUSD"}')
-    greeting = yield from websocket.recv()
-    print("< %s " % greeting)
+    ack = yield from websocket.recv()
+#    print(ack)
+    lasttrades = yield from websocket.recv()
+#    print(lasttrades)
     while True:
-        message = yield from websocket.recv()
-        print(message)
+        raw = yield from websocket.recv()
+        message = json.loads(raw)
+        if message[1] == "tu":
+            print(message[2])
 
 asyncio.get_event_loop().run_until_complete(hello())
-asyncio.get_event_loop().run_forever()
